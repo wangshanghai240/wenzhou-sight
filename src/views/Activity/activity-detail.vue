316ab2a1318ece3dd7543f713a551b97ee4ffe6d
guzy <467019769@qq.com>
2021-06-29 21:36:36 +0800

    活动列表页面

---
+++ src/views/Activity/activity-detail.vue
@@ -0,0 +1,82 @@
+<template>
+	<div class="notice-detail-wrapper">
+	  <div class="notice-info-wrapper">
+	    <div class="notice-info-head-wrapper">
+	      <img alt="" class="notice-bac-img" :src="activity.header">
+	      <span class="notice-title">{{activity.eventname}}</span>
+	    </div>
+	    <div class="notice-content-wrapper">
+	      <div class="notice-content-box" v-html="activity.details"></div>
+	    </div>
+	  </div>
+	</div>
+</template>
+
+<script>
+	import { activityDetail } from '@/api/index'
+	export default {
+		data() {
+			return {
+				activity: {
+					id: 0,
+					eventname: '',
+					header: '',
+					details: ''
+				}
+			}
+		},
+		created() {
+			const id = this.$route.query.id
+			activityDetail(id).then(res => {
+				this.activity = res.data
+			})
+		}
+	}
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+.notice-detail-wrapper{
+  background: #F4F5FB;
+  padding: 30px 0;
+
+
+  .notice-info-wrapper{
+	text-align: left;
+    width: calc(~"100vw - 280px");
+    background: #fff;
+    border-radius: 14px;
+    z-index: 1000;
+    margin-left: 140px;
+
+   .notice-info-head-wrapper{
+     position: relative;
+     height: 400px;
+     overflow: hidden;
+
+     .notice-bac-img{
+       height: 500px;
+       width: calc(~"100% + 100px");
+       object-fit: cover;
+       // -webkit-filter: blur(1px); /* Chrome, Safari, Opera */
+       // filter: blur(1px);
+       position: relative;
+       left: -50px;
+     }
+
+     .notice-title{
+       font-weight: bold;
+       color: #FBFBFD;
+       position: absolute;
+       font-size: 40px;
+       left: 100px;
+       top: 50%;
+       transform: translateY(-50%);
+     }
+   }
+
+    .notice-content-wrapper{
+      padding: 30px 100px;
+    }
+  }
+}
+</style>
