b467e0659575e4b3bb6c29e31b7b150d88fe6710
guzy <467019769@qq.com>
2021-07-12 18:20:28 +0800

    图片上传页面

---
+++ src/components/Common/ImageUploads.vue
@@ -0,0 +1,33 @@
+<!-- <template>
+	<a-upload
+		name="pics"
+		list-type="picture-card"
+		class="picture-uploader"
+		:show-upload-list="true"
+		:file-list="fileList">
+		<div v-if="fileList.length < 8">
+			<a-icon type="plus"/>
+			<div class="ant-upload-text">Upload</div>
+		</div>
+	</a-upload>
+	<a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
+		<img style="width: 100%" :src="previewImage"/>
+	</a-modal>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				previewVisible: false,
+				previewImage: '',
+				fileList: []
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
+ -->
